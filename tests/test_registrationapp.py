566fcd9707f45168e278693234937d4bf58a1059
Spandana-Konda <163428249+Spandana-Konda@users.noreply.github.com>
2025-10-30 15:49:24 +0530

    Update test_registrationapp.py

--- tests/test_registrationapp.py
+++ tests/test_registrationapp.py
@@ -2,6 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.alert import Alert
 from webdriver_manager.chrome import ChromeDriverManager
 import time
@@ -11,29 +12,26 @@ def setup_teardown():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     yield driver
     driver.quit()
-
 def get_alert_text(driver):
     alert = Alert(driver)
     text = alert.text
     alert.accept()
     return text
-
 def test_empty_username(setup_teardown):
     driver = setup_teardown
     driver.get("http://127.0.0.1:5000/")
-
     driver.find_element(By.NAME, "username").clear()
-    driver.find_element(By.NAME, "pwd").send_keys("Password123")
+    driver.find_element(By.NAME, "pwd").send_keys("spapapapapa")
     driver.find_element(By.NAME, "sb").click()
     time.sleep(1)
     alert_text = get_alert_text(driver)
-    assert alert_text == "Username cannot be empty."
+    assert alert_text == "Username noy  empty."
 
 
 def test_password(setup_teardown):
     driver = setup_teardown
     driver.get("http://127.0.0.1:5000/")
-    driver.find_element(By.NAME, "username").send_keys("John Doe")
+    driver.find_element(By.NAME, "username").send_keys("span")
     driver.find_element(By.NAME, "pwd").clear()
     driver.find_element(By.NAME, "sb").click()
     time.sleep(1)
